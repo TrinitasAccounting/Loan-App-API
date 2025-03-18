4c852f7628165168cb03a80001a1e113643d9176
Clay Mangum <clay@trinitasaccounting.com>
2025-03-18 15:58:36 -0400

    fully functional backend

---
+++ loanapp_api/serializers.py
@@ -0,0 +1,12 @@
+
+from rest_framework import serializers
+from .models import Loandata
+
+class LoandataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Loandata
+        fields = '__all__'
+
+
+
+
